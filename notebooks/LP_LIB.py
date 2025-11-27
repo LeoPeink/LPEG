added custom squared loss function and fixed matrix multiplication issues in ridge regression lab solution. added empty gradient function placeholder in LP_LIB.py.

notebooks/LP_LIB.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import numpy.linalg as LA
 
 def rescale(x,new_min,new_max,old_min=0,old_max=1):
     """
@@ -391,3 +391,23 @@ def ridgeRegression(x_train, y_train, lam):
     return w
 
 
+def squaredLoss(X,y,w):
+    """
+    Calcualtes squared loss for linear model with weights w on data X with targets y.
+    Parameters
+    ----------
+    X : array
+        Input data
+    y : array
+        Output data
+    w : array
+        Weights of the linear model
+    Returns
+    -------
+    float
+        Squared loss value 
+    """
+    return (np.linalg.norm((y-X@w),2)**2)/len(X)
+
+def gradientDescent():
+    return

notebooks/Lab_3.SOL.py
@@ -7,9 +7,7 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
 
-#generate linear (albeit noisy) data for linear regression.
-
-n = 50000
+n = 5000
 dim = 1
 l = 0
 u = 1
@@ -17,80 +15,63 @@ w = None
 q=0
 sigma = 1
 global_seed = 1
-
 np.random.seed(global_seed)
-d = lp.linDataGen(n,dim,l,u,w,q,sigma,True)
-X = d[0]
-print(X)
-y = d[1]
-print(y)
-y_true = d[2]
 
-#create an array for weights
-ws = []
+
+d = lp.linDataGen(n,dim,l,u,w,q,sigma,True) #generate linear data with noise
+X = d[0]    #feature matrix, input
+# print(X)
+y = d[1]    #output vector, target with noise
+# print(y)
+y_true = d[2]   #ground truth output vector, target without noise
+
+#create an array for weights (ridge regression)
+weights_rr = []
 lams = []
 n_models = 10
 
-#arrays to store MLE errors
+#arrays to store MLE errors (ridge regression)
 MLE_TR = []
 MLE_TE = []
 
 
-#implement linear regression in closed form
-
-#w = (XX^t)^-1 XY^t
-#w = inv(X.T @ X) @ X.T @ y
-
-
+#split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=global_seed)
-print(X_train)
-print(y_train)
-
-
 
 
 plt.scatter(X_train,y_train, label = 'Training data')
 plt.scatter(X_test,y_test, label = 'Testing data')
 
-wl = lp.linearRegression(X_train,y_train)
+weight_lr = lp.linearRegression(X_train,y_train)                #compute linear regression weights (lr model)
 
-#xp = np.linspace(l, u, 100)
-
-
-#create an array for weights
-ws = []
-lams = []
-n_models = 10
 
 for i in range(n_models):
     #lam = n_models/(2*i+1) #select lambda values decreasing
-    lam = np.linspace(0.01,100000,n_models)[i]
+    lam = np.linspace(0.01,100,n_models)[i]
     w = lp.ridgeRegression(X_train,y_train,lam) #compute ridge regression weights
-    ws.append(w)    #add weight to array
+    weights_rr.append(w)    #add weight to array
     lams.append(lam)  #add lambda to array
     yp = w * X_train
-    #use gradient to plot clearly every different model
-    
     plt.plot(X_train, yp, c=plt.cm.plasma(i/n_models), alpha = 0.75,label='RR lambda=%.2f' % lam)
     MLE_TR.append(mean_squared_error(y_train, yp))    #training error
     
     
 plt.plot(X,y_true, c='green', label='Ground truth')
-plt.plot(X_train, X_train * wl, c='red', label='Linear regression')    
+plt.plot(X_train, X_train @ weight_lr, c='red', label='Linear regression')    
 #evaluate models on test set
 for i in range(n_models):
-    MLE_TE.append(mean_squared_error(y_test, X_test * ws[i]))    #testing error
+    MLE_TE.append(mean_squared_error(y_test, X_test * weights_rr[i]))    #testing error
     
     
     
 
-print("Estimated w:")
-print(w)
+#print("Estimated w:")
+#print(w)
 plt.legend()
 
 
 plt.figure()
-plt.scatter(lams,ws, label='weight over lambda value')
+plt.scatter(lams,weights_rr, label='weight over lambda value')
 #plt.xscale('log')
 plt.xlabel('lambda')
 plt.ylabel('w')
@@ -108,6 +89,12 @@ plt.ylabel('Mean Squared Error')
 plt.legend()
 plt.show()
 
+'''
+linerr = mean_squared_error(y_train, weight_lr@X_train)
+sqtest = lp.squaredLoss(X_train,y_train,weight_lr)
+print(sqtest)
+print(linerr)
+'''
 
 
 """